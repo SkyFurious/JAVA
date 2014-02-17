a886377f0b70d5c3f7affeb95331592ea0cd459d
Dean Winchester <dean.winchester.mind@gmail.com>
2014-02-17 12:42:57 +0100

    Update resolutionEquation.java

--- resolutionEquation.java
+++ resolutionEquation.java
@@ -1,24 +1,24 @@
 /**
  * \file resolutionEquation.java
- * \brief Programme pour r�soudre une �quation
+ * \brief Programme pour résoudre une équation
  * \author Dean Winchester
  * \version 0.1
- * \date 13 F�vrier 2014
+ * \date 13 Février 2014
  * javac resolutionEquation.java
  * java resolutionEquation
  */
 import java.util.*;
 /**
  * \class resolutionEquation
- * \brief resous une �quation de type ax�+bx+c
+ * \brief resous une équation de type ax+bx+c
  * 
- * resolutionEquation est une petite classe pour resoudre une �quation de type ax�+bx+c
+ * resolutionEquation est une petite classe pour resoudre une équation de type ax+bx+c
  */
 class resolutionEquation {
 	
 	public static void main(String args[]) throws Exception {
 		double root, root1, root2;
-		System.out.println("Equation est ax�+bx+c");
+		System.out.println("Equation est ax+bx+c");
 		Integer a = new Integer(args[0]);
 		Integer b = new Integer(args[1]);
 		Integer c = new Integer(args[2]);
@@ -26,11 +26,11 @@ class resolutionEquation {
 		if (d > 0) {
 			root1 = ((-b) + Math.sqrt(d)) / (2 * a);
 			root2 = ((-b) - Math.sqrt(d)) / (2 * a);
-			System.out.println("Deux Solutions a l'�quation :");
+			System.out.println("Deux Solutions a l'équation :");
 			System.out.println(root1 + " , " + root2);
 		} else if (d == 0) {
 			root = (-b) / (2 * a);
-			System.out.println("Une Seul Solution a l'�quation :");
+			System.out.println("Une Seul Solution a l'équation :");
 			System.out.println(root);
 		} else if (d < 0) {
 			System.out.println("La solution est imaginaire");
