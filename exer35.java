2525ed651eb89c519e2177216cca7e031b19e78e
Dean Winchester <dean.winchester.mind@gmail.com>
2014-03-03 10:54:01 +0100

    Update exer35.java

--- exer35.java
+++ exer35.java
@@ -1,25 +1,25 @@
 /**
- * Titre :        Exercise 35
- * Description :  Annuaire téléphonique
- * Copyright :    Copyright (c) 2002
- * Société :
- * @author SN
- * @version 1.0
- */
-
+* \file exer35.java
+* \brief Programme pour gÃ©rer des tableaux de tableaux de doubles.
+* \author Dean Winchester
+* \version 0.1
+* \date 02 Mars 2014
+* javac exer35.java
+* java exer35
+*/
 public class exer35 {
         public static void main(String[] args)
 	{
 		Index ind=new Index(10);
-		System.out.println("There are "+ind.getNbSubscribers()+" subscribers");
-		Subscriber a=new Subscriber("Bob","111");
-		Subscriber b=new Subscriber("Bill","1121");
+		System.out.println("Il y a "+ind.getNbSubscribers()+" abonnÃ©es");
+		Subscriber a=new Subscriber("Steve","114");
+		Subscriber b=new Subscriber("Yoan","1123");
 		ind.addSubscriber(a);
 		ind.addSubscriber(b);
-		ind.addSubscriber(new Subscriber("Jack","211"));
-		ind.addSubscriber(new Subscriber("John","311"));
-		System.out.println("There are "+ind.getNbSubscribers()+" subscribers");
-		System.out.println("who are :");
+		ind.addSubscriber(new Subscriber("Valentin","3211"));
+		ind.addSubscriber(new Subscriber("Michael","3114"));
+		System.out.println("Il y a "+ind.getNbSubscribers()+" abonnÃ©es");
+		System.out.println("Vous etes :");
 		for (int i=0; i<ind.getNbSubscribers(); i++)
 			System.out.println(ind.getSubscriber(i).getName()+" "+
 			                   ind.getSubscriber(i).getNumber());
