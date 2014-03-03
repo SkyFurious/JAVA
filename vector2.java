70c6ef22781af2a72f0a7c17409da4b26060a6e7
Dean Winchester <dean.winchester.mind@gmail.com>
2014-03-03 12:24:16 +0100

    Update vector2.java

--- vector2.java
+++ vector2.java
@@ -1,4 +1,4 @@
-/**
+*/**
 * \file vector.java
 * \brief Programme pour faire des opérations sur des vecteurs de 3 composantes
 * \author Dean Winchester
@@ -43,6 +43,8 @@ class Vector {
 		v1.x=v2.x; v1.y=v2.y; v1.z=v2.z;
 		v2.x=t.x; v2.y=t.y; v2.z=t.z;
 	}
+}
+public class vector2 {
         public static void main(String[] args) {
         	// on crée 2 vecteur
 		Vector v1=new Vector(1,2,3);
